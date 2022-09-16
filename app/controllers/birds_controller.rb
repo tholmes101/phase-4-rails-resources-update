cb634c1917884f9e526cf088a0ff32540ed3e656
Terence Holmes <piece4ulmind@gmail.com>
2022-09-15 22:50:16 -0400

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -28,4 +28,25 @@ class BirdsController < ApplicationController
     params.permit(:name, :species)
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
 end
